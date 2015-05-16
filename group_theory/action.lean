aee71962d66a0b8acea75c70974836ecfcc0f157
Haitao Zhang <zht@nocontact.org>
2015-05-15 22:26:32 -0700

    fiexed perm.one_mul

--- group_theory/action.lean
+++ group_theory/action.lean
@@ -3,7 +3,7 @@ import algebra.group data.set data.fintype .extra
 namespace algebra
 namespace group
 section perm
-open set function
+open set function eq.ops
 variable {A : Type}
 structure perm (A : Type) : Type :=
 (f : A → A) (is_perm : @bijective A A f)
@@ -16,12 +16,11 @@ local infix `^` := perm.compose
 theorem perm.assoc (h g f : perm A) : h ^ g ^ f = h ^ (g ^ f) := rfl
 reveal perm.assoc
 
+check @eq.rec_on
 definition perm.one : perm A := perm.mk id id_is_bij
 lemma perm.one_mul (f : perm A) : perm.one ^ f = f :=
-  begin
-  esimp [perm.compose, perm.one],
-  rewrite (left_id f)
-  end
+      perm.cases_on f (λ f Hf, rfl)
+
 -- to construct an inv we need to go finite
 example : semigroup (perm A) := @semigroup.mk (perm A) perm.compose perm.assoc
 end perm
@@ -30,7 +29,7 @@ variable {A : Type}
 variable [FinA : fintype A]
 include FinA
 
-definition find_inv (f : perm A) : A → A :=
+definition find_inv (f : perm A) : A → A := sorry
   
 definition perm.inv (f : perm A) : perm A := sorry
   
