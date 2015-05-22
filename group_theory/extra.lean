f3323e2d8b085e5d697fd0c1fa4e54012ee9515c
Haitao Zhang <zht@nocontact.org>
2015-05-21 18:45:14 -0700

    theory of partition class equation

--- group_theory/extra.lean
+++ group_theory/extra.lean
@@ -5,7 +5,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Author : Haitao Zhang
 -/
 -- These belong in the library somewhere.
-import algebra.group data.nat data.set data.list data.finset
+import algebra.group data
 
 -- Thought this might be useful in converting different forms of Prop
 theorem and_imp_curry (a b c : Prop) : (a ∧ b → c) = (a → b → c) :=
@@ -142,6 +142,42 @@ lemma image_eq_Union_index_image (s : finset A) (f : A → finset B) : Union s f
       (take s1 a Pa IH, by rewrite [image_insert, *Union_insert, IH])
 
 end image
+
+section eqv
+open function
+variable {A : Type}
+variable [deceqA : decidable_eq A]
+include deceqA
+
+definition is_eqv_class (f : A → finset A) := ∀ a b, a ∈ f b = (f a = f b)
+definition is_restricted_cover (s : finset A) (f : A → finset A) := s = Union s f
+structure partition : Type :=
+(set : finset A) (part : A → finset A) (eqv : is_eqv_class part)
+      (complete : is_restricted_cover set part)
+definition eqv_classes (f : partition) : finset (finset A) := image (partition.part f) (partition.set f)
+
+lemma eqv_class_disjoint (f : partition) (a1 a2 : finset A) (Pa1 : a1 ∈ eqv_classes f) (Pa2 : a2 ∈ eqv_classes f) : a1 ≠ a2 → a1 ∩ a2 = ∅ :=
+      assume Pne,
+      assert Pe1 : _, from exists_of_mem_image Pa1, obtain g1 Pg1, from Pe1,
+      assert Pe2 : _, from exists_of_mem_image Pa2, obtain g2 Pg2, from Pe2,
+      begin
+      apply inter_eq_empty_of_disjoint,
+      apply disjoint.intro,
+      rewrite [eq.symm (and.right Pg1), eq.symm (and.right Pg2)],
+      intro x,
+      rewrite [*(partition.eqv f)],
+      intro Pxg1, rewrite [Pxg1, and.right Pg1, and.right Pg2],
+      intro Pe, exact absurd Pe Pne      
+      end
+lemma class_equation (f : @partition A _) : card (partition.set f) = nat.Sum (eqv_classes f) card := calc
+      card (partition.set f) = card (Union (partition.set f) (partition.part f)) : partition.complete f
+      ... = card (Union (image (partition.part f) (partition.set f)) id) : image_eq_Union_index_image (partition.set f) (partition.part f)
+      ... = card (Union (eqv_classes f) id) : rfl
+      ... = nat.Sum (eqv_classes f) card : card_Union_of_disjoint _ id (eqv_class_disjoint f)
+
+local attribute partition.part [coercion]
+
+end eqv
 end finset
 
 namespace list
